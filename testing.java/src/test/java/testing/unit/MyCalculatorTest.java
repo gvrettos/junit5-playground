eb2e7a937ccc6423c44a1d2c73945f91e5502a54
George Vrettos <lukedevgr@gmail.com>
2019-08-11 20:26:08 +0300

    Add basic junit 5 tests

--- testing.java/src/test/java/testing/unit/MyCalculatorTest.java
+++ testing.java/src/test/java/testing/unit/MyCalculatorTest.java
@@ -1,14 +1,34 @@
 package testing.unit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import testing.junit.MyCalculator;
+
 class MyCalculatorTest {
+	
+	private MyCalculator myCalculator;
+	
+	@BeforeEach
+	void setUp() {
+		myCalculator = new MyCalculator();
+	}
 
 	@Test
-	void testDivide() {
-		fail("Not yet implemented");
+	void testDivideDividendLargerThanDivisor() {
+		assertEquals(4, myCalculator.divide(100, 25));
+	}
+	
+	@Test
+	void testDivideDivisorLargerThanDividend() {
+		assertEquals(0, myCalculator.divide(25, 100));
+	}
+	
+	@Test
+	void testDivideDividendEqualToDivisor() {
+		assertEquals(1, myCalculator.divide(999, 999));
 	}
 
 }
