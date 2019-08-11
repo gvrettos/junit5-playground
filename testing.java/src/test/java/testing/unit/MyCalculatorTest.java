5d5c50b42f094b94856fa43ba565974116cfba11
George Vrettos <lukedevgr@gmail.com>
2019-08-11 20:29:54 +0300

    Add performance unit testing

--- testing.java/src/test/java/testing/unit/MyCalculatorTest.java
+++ testing.java/src/test/java/testing/unit/MyCalculatorTest.java
@@ -1,7 +1,9 @@
 package testing.unit;
 
+import static java.time.Duration.ofMillis;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,5 +38,14 @@ class MyCalculatorTest {
 	void testDivideByZero() {
 		Throwable exception = assertThrows(ArithmeticException.class, () -> myCalculator.divide(999, 0));
 	    assertEquals("/ by zero", exception.getMessage());
+	}
+	
+	@Test
+	void testDividePerformance() {
+		assertTimeout(ofMillis(1), () -> {
+			for (int i=4; i<10000 ;i++) {
+				myCalculator.divide(i*3, i-3);
+			}
+		});
 	}
 }
